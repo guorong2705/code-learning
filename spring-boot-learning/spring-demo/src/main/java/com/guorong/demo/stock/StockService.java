830405d75ea2563a71c3c1e3d48abc5dc929c754
guorong2705 <guorong2705@foxmail.com>
2024-05-21 22:00:30 +0800

    update

--- spring-boot-learning/spring-demo/src/main/java/com/guorong/demo/stock/StockService.java
+++ spring-boot-learning/spring-demo/src/main/java/com/guorong/demo/stock/StockService.java
@@ -6,17 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
 @Service
 public class StockService {
 
     @Autowired
     private DbStockMapper dbStockMapper;
 
-    // @Transactional(rollbackFor = Exception.class)
     public synchronized void reduceStock() {
         DbStock dbStock = dbStockMapper.selectCustomById(1L);
         dbStock.setCount(dbStock.getCount() -1);
         dbStockMapper.updateById(dbStock);
-        System.out.println(dbStock.getProductCode() + " 库存余量: " + dbStock.getCount());
     }
 }
